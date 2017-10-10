5b4a083a6c94129871406c50dddc69ee24ba8f12
wahyumustajib69 <adjieb.wahyu@gmail.com>
2017-10-10 19:26:30 +0800

    Angka

---
+++ anngkadanlooping.cpp
@@ -0,0 +1,38 @@
+#include<iostream.h>
+#include<conio.h>
+
+void main(){
+	int pil,angka,a,b,c;
+	clrscr();
+	cout<<"===== PILIHAN =====\n";
+	cout<<"[1] GANJIL GENAP\n";
+	cout<<"[2] LOOPING\n";
+	cout<<"[3] EXIT\n";
+	cout<<"\n";
+	
+	cout<<"MASUKKAN PILIHAN	: ";cin>>pil;
+	switch(pil){
+		case 1:
+			cout<<"MASUKKAN ANGKA : ";cin>>angka;
+			if (angka==0)
+				cout<<"NILAI KOSONG";
+			else if(angka %2==0)
+				cout<<"NILAI GENAP";
+			else
+				cout<<"ANGKA GANJIL";
+				break;
+		case 2:
+			cout<<"MASUKKAN ANGKA : ";cin>>a;
+			c=1;
+			for (b=1;b<=a+1;b++){
+				cout<<c<<"";
+				c=b+c;
+			}
+			break;
+		case 3:
+			break;
+			default:
+				cout<<"TIDAK ADA PILIHAN!!";
+	}
+	getch();
+}
